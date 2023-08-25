7a4b886f28d9e10a765e0380171b261fd0402863
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-25 09:50:37 -0300

    popup loging

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState }  from "react";
+import './App.css'
+function PopUp(){
+    const [popup,setPop]=useState(false)
+    const handleClickOpen=()=>{
+        setPop(!popup)
+    }
+    const closePopup=()=>{
+        setPop(false)
+    }
+    return(
+        <div>
+            <button onClick={handleClickOpen}>Open popup</button>
+            <div>
+                {
+                    popup?
+                    <div className="main">
+                        <div className="popup">
+                            <div className="popup-header">
+                                <h1>popup</h1>
+                                <h1 onClick={closePopup}>X</h1>
+                            </div>
+                            <div>
+                            <p>This is simple popup in React js</p>
+                            </div>
+                        </div>
+                    </div>:""
+                }
+            </div>
+        </div>
+    )
 }
-
-export default App;
+export default PopUp;
